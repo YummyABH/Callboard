7ec6cffdf18237942985d7c9f6b8f45e81dd5e5b
nikita.sidiropulo@a-mobile.biz <Yomo1Yomo>
2025-02-13 11:25:47 +0300

    добавлена функциональная кнопка авторизации в телегам

---
+++ src/components/auth/TelegramAuthButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const telegramContainer = ref(null);
+
+onMounted(() => {
+  if (telegramContainer.value) {
+    const script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-widget.js?21";
+    script.setAttribute("data-telegram-login", "AbhCallboardBot");
+    script.setAttribute("data-size", "large");
+    script.setAttribute("data-auth-url", "https://www.dns-shop.ru/catalog/17a8a58c16404e77/audiokolonki/?order=6&stock=now-today-tomorrow-later&f%5B11%5D=4&utm_referrer=https%3A%2F%2Fwww.dns-shop.ru%2Fcatalog%2F17a8925616404e77%2Fkolonki%2F");
+    script.setAttribute("data-request-access", "write");
+    script.async = true;
+    telegramContainer.value.appendChild(script);
+  }
+});
+</script>
+
+<template>
+        <div class="flex items-center" ref="telegramContainer"></div>
+</template>
--- src/page/AuthPage.vue
+++ src/page/AuthPage.vue
@@ -1,4 +1,5 @@
 <script setup>
+import TelegramAuthButton from "@/components/auth/TelegramAuthButton.vue";
 // import LoginForm from '@/components/auth/LoginForm.vue'
 // import RegisterForm from '@/components/auth/RegisterForm.vue'
 // import modalConfirmationCode from '@/components/auth/modalConfirmationCode.vue'
@@ -13,50 +14,13 @@
 // const goToRegister = () => {
 //   router.push({ name: 'register' })
 // }
-
-import { onMounted } from 'vue';
-
-const botUsername = 'samplebot'; // Замени на своего бота
-
-// Функция обработки авторизации
-const onTelegramAuth = (user) => {
-  alert(`Logged in as ${user.first_name} ${user.last_name} (${user.id}${user.username ? ', @' + user.username : ''})`);
-};
-
-onMounted(() => {
-  // Добавляем скрипт Telegram, если его нет
-  if (!document.getElementById('telegram-widget-script')) {
-    const script = document.createElement('script');
-    script.id = 'telegram-widget-script';
-    script.src = 'https://telegram.org/js/telegram-widget.js?22';
-    script.async = true;
-    document.body.appendChild(script);
-  }
-
-  // Создаём контейнер для Telegram Login Widget
-  const container = document.getElementById('telegram-login-container');
-  if (container) {
-    container.innerHTML = ''; // Очищаем контейнер перед вставкой нового виджета
-
-    const widget = document.createElement('script');
-    widget.src = 'https://telegram.org/js/telegram-widget.js?22';
-    widget.setAttribute('data-telegram-login', botUsername);
-    widget.setAttribute('data-size', 'large');
-    widget.setAttribute('data-onauth', 'onTelegramAuth(user)');
-    widget.setAttribute('data-request-access', 'write');
-    widget.async = true;
-    container.appendChild(widget);
-  }
-
-  // Делаем функцию доступной в глобальном контексте
-  window.onTelegramAuth = onTelegramAuth;
-});
 </script>
 
 
 
 <template>
-  <div class="relative bg-[#353535] w-sceen h-screen flex justify-center" style=" background-image: url(/public/pattern2.svg)">
+  <div class="relative bg-[#353535] w-sceen h-screen flex justify-center"
+    style=" background-image: url(/public/pattern2.svg)">
     <!-- <modalConfirmationCode class="absolute rounded z-10 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"/> -->
     <!-- <div class="flex m-auto flex-col w-1/4 px-16 py-20 bg-[#04041f] rounded-xl">
       <h1 class="text-center mb-4 text-2xl" :class="[$route.name === 'login' ? 'hidden' : '']">
@@ -74,9 +38,6 @@ onMounted(() => {
         <RegisterForm v-else />
       </div>
     </div> -->
-  
-    <!-- Telegram Login Widget -->
-    <div id="telegram-login-container">asdasdasd</div>
-    
+    <TelegramAuthButton/>
   </div>
 </template>
